938112b43421a2ba16a0e280061848588f362a1b
Neto <delcides.c.c.neto@gmail.com>
2025-10-14 10:31:46 -0300

    Alterações sem utilidade

--- teste.py
+++ teste.py
@@ -1,14 +1,2 @@
-import json
-def criarJson():
-    jsonInicial = {'links': []}
-    with open('links.json','wt+') as arqivoJson:
-        json.dump(jsonInicial, arqivoJson, indent=4)    
-
-
-def adicionarLinks(link):
-    with open('links.json', 'r', encoding='utf-8') as arquivoJson:
-        linksJson = json.load(arquivoJson)
-        linksJson['links'].append(link)
-        with open('links.json','wt+') as links:
-            json.dump(linksJson, links, indent=4)
-criarJson()
+import requests
+requests.get('https://nfeweb.sefaz.go.gov.br/nfeweb/sites/nfe/consulta-publica/resultado/download/historico/arquivo/27410584000120_01022025_28022025_5613.zip')
